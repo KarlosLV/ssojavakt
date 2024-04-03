34a83b26fc3f07311e7961321caa3e60e43ba25f
Uno <juangabrielmina1220@gmail.com>
2024-04-02 20:25:39 -0700

    anodawan

---
+++ jgmina/ssosim/src/test/java/com/apikt/ssosim/SsosimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apikt.ssosim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsosimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
