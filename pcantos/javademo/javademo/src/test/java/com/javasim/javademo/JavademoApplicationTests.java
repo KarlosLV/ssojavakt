3f457f07f2a5c67626d9b844a9375f981d9640fe
Psyla Louise Cantos <psyla.cantos@gmail.com>
2024-04-03 11:39:59 +0800

    ty

---
+++ pcantos/javademo/javademo/src/test/java/com/javasim/javademo/JavademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javasim.javademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
