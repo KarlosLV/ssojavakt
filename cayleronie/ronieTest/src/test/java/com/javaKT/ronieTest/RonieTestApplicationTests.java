b3404a74ed6fd89d8ba0aed6a1c8603a18f965ce
cayleronie <cayleronie.r@gmail.com>
2024-04-03 11:31:23 +0800

    Move folder

---
+++ cayleronie/ronieTest/src/test/java/com/javaKT/ronieTest/RonieTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaKT.ronieTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonieTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
