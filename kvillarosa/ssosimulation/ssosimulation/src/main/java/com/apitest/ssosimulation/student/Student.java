51c47a819f5a3a2d31693f52f9da82a7ec03af7b
Karlos Lucas Villarosa <zxckarlos@gmail.com>
2024-04-05 15:04:54 +0800

    hello

--- kvillarosa/ssosimulation/ssosimulation/src/main/java/com/apitest/ssosimulation/student/Student.java
+++ kvillarosa/ssosimulation/ssosimulation/src/main/java/com/apitest/ssosimulation/student/Student.java
@@ -1,20 +1,54 @@
 package com.apitest.ssosimulation.student;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
+
+    @Transient
     private Integer age;
 
-    public Student(Long id, String name, String email, LocalDate dob, Integer age) {
+    public Student() {
+
+    }
+
+    public Student(
+            String name,
+            String email,
+            LocalDate dob) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+    }
+
+    public Student(
+            Long id,
+            String name,
+            String email,
+            LocalDate dob) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.dob = dob;
-        this.age = age;
     }
 
     public Long getId() {
@@ -50,9 +84,9 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
+//        return age;
     }
-
     public void setAge(Integer age) {
         this.age = age;
     }
@@ -67,4 +101,6 @@ public class Student {
                 ", age=" + age +
                 '}';
     }
+
+
 }
---
+++ kvillarosa/ssosimulation/ssosimulation/src/main/java/com/apitest/ssosimulation/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.apitest.ssosimulation.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student lucas = new Student(
+                    1L,
+                    "Karlos Lucas Villarosa",
+                    "k.lucas.villarosa@gmail.com",
+                    LocalDate.of(1999, Month.FEBRUARY, 7)
+            );
+
+            Student mikaela = new Student(
+                    2L,
+                    "Mikaela B. Kaligayahan",
+                    "mikaela_kaligayahan@gmail.com",
+                    LocalDate.of(1999, Month.NOVEMBER, 29)
+            );
+            repository.saveAll(
+                    List.of(lucas,mikaela)
+            );
+        };
+
+    }
+
+}
