706f476e283c4a2fa7f748fd4e45a309aef8cf9b
iamenrique03 <enriquetumanganjr@gmail.com>
2024-04-08 09:22:23 +0800

    Olaaaa

---
+++ etumangan/KT_SpringBoot/KT_SpringBoot/src/main/java/com/example/KT_SpringBoot/KtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.KT_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtSpringBootApplication.class, args);
+	}
+
+}
---
+++ etumangan/KT_SpringBoot/KT_SpringBoot/src/test/java/com/example/KT_SpringBoot/KtSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KT_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
