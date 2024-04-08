3f819785fbc1bce35ec2b9287c90c806025a815c
cayleronie <cayleronie.r@gmail.com>
2024-04-08 10:24:24 +0800

    yown

--- cayleronie/ronieTest/src/main/java/com/javaKT/ronieTest/RonieTestApplication.java
+++ cayleronie/ronieTest/src/main/java/com/javaKT/ronieTest/RonieTestApplication.java
@@ -2,12 +2,19 @@ package com.javaKT.ronieTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RonieTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RonieTestApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String helloworld() {
+		return "hello";
+	}
 }
